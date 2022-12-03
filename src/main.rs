bad18f8291d3d425f7bf1a58d99da9281932d4c4
Joel Lidin <joellidin@gmail.com>
2022-12-03 13:20:16 +0100

    Add solution for day 3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,6 +9,7 @@ fn main() {
     match day_solution.as_str() {
         "1" => day1::solution(),
         "2" => day2::solution(),
+        "3" => day3::solution(),
         _ => panic!("Not a valid day for advent of code."),
     };
 }
