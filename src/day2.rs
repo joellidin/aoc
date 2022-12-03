9c6de7dae75f7929bac126e88cea06b345a47a26
Joel Lidin <joel.lidin@volvocars.com>
2022-12-03 15:10:56 +0100

    Refactor solution for day 2
    Use a enum for Rock, Paper and Scissors with some implementations for
    ordering and two methods for getting the loser shape and winner shape,
    respectively.

--- src/day2.rs
+++ src/day2.rs
@@ -1,62 +1,96 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::prelude::BufRead;
 use std::io::BufReader;
+use std::str::FromStr;
+use Shape::*;
+
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
+enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl PartialOrd for Shape {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+            (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) => Some(Ordering::Less),
+            (Scissors, Paper) | (Paper, Rock) | (Rock, Scissors) => Some(Ordering::Greater),
+            (_, _) => Some(Ordering::Equal),
+        }
+    }
+}
+
+impl From<Shape> for u32 {
+    fn from(shape: Shape) -> Self {
+        match shape {
+            Rock => 1,
+            Paper => 2,
+            Scissors => 3,
+        }
+    }
+}
+
+impl FromStr for Shape {
+    type Err = std::convert::Infallible;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Rock),
+            "B" | "Y" => Ok(Paper),
+            "C" | "Z" => Ok(Scissors),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Shape {
+    fn get_winner_shape(&self) -> Shape {
+        match self {
+            Rock => Scissors,
+            Paper => Rock,
+            Scissors => Paper,
+        }
+    }
+
+    fn get_loser_shape(&self) -> Shape {
+        match self {
+            Rock => Paper,
+            Paper => Scissors,
+            Scissors => Rock,
+        }
+    }
+}
 
 pub fn solution() {
-    let mut winner_map = HashMap::new();
-    winner_map.insert("A".to_owned(), "Y".to_owned());
-    winner_map.insert("B".to_owned(), "Z".to_owned());
-    winner_map.insert("C".to_owned(), "X".to_owned());
-
-    let mut equal_map = HashMap::new();
-    equal_map.insert("A".to_owned(), "X".to_owned());
-    equal_map.insert("B".to_owned(), "Y".to_owned());
-    equal_map.insert("C".to_owned(), "Z".to_owned());
-
-    let mut scores = HashMap::new();
-    scores.insert("X".to_owned(), 1);
-    scores.insert("Y".to_owned(), 2);
-    scores.insert("Z".to_owned(), 3);
-    scores.insert("A".to_owned(), 1);
-    scores.insert("B".to_owned(), 2);
-    scores.insert("C".to_owned(), 3);
-
-    let mut winner_map2 = HashMap::new();
-    winner_map2.insert("A".to_owned(), "B".to_owned());
-    winner_map2.insert("B".to_owned(), "C".to_owned());
-    winner_map2.insert("C".to_owned(), "A".to_owned());
-
-    let mut loser_map = HashMap::new();
-    loser_map.insert("A".to_owned(), "C".to_owned());
-    loser_map.insert("B".to_owned(), "A".to_owned());
-    loser_map.insert("C".to_owned(), "B".to_owned());
-
     let file = File::open("data/day2.txt").unwrap();
     let reader = BufReader::new(file);
-
     let result: u32 = reader.lines().fold(0, |acc, line| {
         let line = line.unwrap();
         let (a, b) = line.split_once(" ").unwrap();
-        let mut score = 0;
-        if winner_map.get(a).unwrap() == b {
-            score += 6;
-        } else if equal_map.get(a).unwrap() == b {
-            score += 3;
-        }
-        acc + scores.get(b).unwrap() + score
+        let (a, b) = (Shape::from_str(a).unwrap(), Shape::from_str(b).unwrap());
+        let score = if a < b {
+            6
+        } else if a == b {
+            3
+        } else {
+            0
+        };
+        acc + u32::from(b) + score
     });
     println!("The total score is {}", result);
 
+    // Part 2
     let file = File::open("data/day2.txt").unwrap();
     let reader = BufReader::new(file);
     let result: u32 = reader.lines().fold(0, |acc, line| {
         let line = line.unwrap();
         let (a, b) = line.split_once(" ").unwrap();
+        let a = Shape::from_str(a).unwrap();
         let score = match b {
-            "X" => *scores.get(loser_map.get(a).unwrap()).unwrap(),
-            "Y" => 3 + scores.get(a).unwrap(),
-            "Z" => 6 + *scores.get(winner_map2.get(a).unwrap()).unwrap(),
+            "X" => u32::from(a.get_winner_shape()),
+            "Y" => 3 + u32::from(a),
+            "Z" => 6 + u32::from(a.get_loser_shape()),
             _ => panic!("NO MATCH"),
         };
         acc + score
