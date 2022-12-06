7fcb3da8f5860e40d812a8046eb519dc9d23ee8e
Joel Lidin <joellidin@gmail.com>
2022-12-06 16:42:41 +0100

    Refactor solution for day 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,92 +1,91 @@
 use std::str::FromStr;
 
-#[derive(Debug)]
-struct Stacks {
-    stacks: Vec<Vec<String>>,
+#[derive(Debug, Clone)]
+struct Dock {
+    stacks: Vec<Vec<char>>,
 }
 
-impl FromStr for Stacks {
+impl FromStr for Dock {
     type Err = std::convert::Infallible;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let lines: Vec<&str> = s.split('\n').collect();
-        let size = lines.len();
-        let n_stacks = (lines[size - 1].len() as f32 / 4.0 + 1.0) as usize;
-        let mut stacks: Vec<Vec<String>> = Vec::new();
-        for i in (0..size - 1).rev() {
-            for j in 1..n_stacks + 1 {
-                match stacks.get(j - 1) {
-                    Some(_) => (),
-                    None => stacks.push(Vec::new()),
-                }
-                match lines[i].get(j * 4 - 3..j * 4 - 2) {
-                    Some(c) => {
-                        if c != " " {
-                            stacks[j - 1].push(c.to_owned())
-                        }
-                    }
-                    None => (),
+        let lines = s
+            .split('\n')
+            .collect::<Vec<_>>()
+            .iter()
+            .rev()
+            .skip(1)
+            .map(|line| line.chars().skip(1).step_by(4).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        let mut stacks = vec![Vec::<char>::new(); lines[0].len()];
+        for line in lines.iter() {
+            for (i, c) in line.iter().enumerate() {
+                if *c != ' ' {
+                    stacks[i].push(*c);
                 }
             }
         }
-        Ok(Stacks { stacks })
+        Ok(Dock { stacks })
     }
 }
 
-impl Stacks {
-    fn move_crates(&mut self, n_crates: usize, from_stack: usize, to_stack: usize) {
-        for _ in 0..n_crates {
-            let item = self.stacks[from_stack - 1].pop().unwrap();
-            self.stacks[to_stack - 1].push(item);
+impl Dock {
+    fn use_crate_mover_9000(&mut self, instructions: &str) {
+        for instruction in instructions.trim().split("\n") {
+            let (n_crates, from_stack, to_stack) = Dock::parse_instruction(instruction);
+
+            for _ in 0..n_crates {
+                let item = self.stacks[from_stack - 1].pop().unwrap();
+                self.stacks[to_stack - 1].push(item);
+            }
         }
     }
 
-    fn move_crates2(&mut self, n_crates: usize, from_stack: usize, to_stack: usize) {
-        let end = self.stacks[from_stack - 1].len();
-        let items: Vec<_> = self.stacks[from_stack - 1].drain(end-n_crates..end).collect();
-        for item in items {
-            self.stacks[to_stack - 1].push(item.to_owned());
+    fn use_crate_mover_9001(&mut self, instructions: &str) {
+        for instruction in instructions.trim().split("\n") {
+            let (n_crates, from_stack, to_stack) = Dock::parse_instruction(instruction);
+            let end = self.stacks[from_stack - 1].len();
+            let items: Vec<_> = self.stacks[from_stack - 1]
+                .drain(end - n_crates..end)
+                .collect();
+            for item in items {
+                self.stacks[to_stack - 1].push(item.to_owned());
+            }
         }
+    }
+
+    fn parse_instruction(instruction: &str) -> (usize, usize, usize) {
+        let (_, split) = instruction.split_once("move ").unwrap();
+        let (n_crates, split) = split.split_once(" ").unwrap();
+        let n_crates: usize = n_crates.parse().unwrap();
+        let (_, split) = split.split_once("from ").unwrap();
+        let (from_stack, split) = split.split_once(" ").unwrap();
+        let from_stack: usize = from_stack.parse().unwrap();
+        let (_, to_stack) = split.split_once("to ").unwrap();
+        let to_stack: usize = to_stack.parse().unwrap();
+
+        (n_crates, from_stack, to_stack)
     }
 }
 
 pub fn solution() {
     let input = std::fs::read_to_string("data/day5.txt").unwrap();
-    let (stacks, commands): (&str, &str) = input.split_once("\n\n").unwrap();
-    let mut stacks: Stacks = stacks.parse().unwrap();
-    for command in commands.trim().split("\n") {
-        let (_, split) = command.split_once("move ").unwrap();
-        let (n_crates, split) = split.split_once(" ").unwrap();
-        let n_crates: usize = n_crates.parse().unwrap();
-        let (_, split) = split.split_once("from ").unwrap();
-        let (from_stack, split) = split.split_once(" ").unwrap();
-        let from_stack: usize = from_stack.parse().unwrap();
-        let (_, to_stack) = split.split_once("to ").unwrap();
-        let to_stack: usize = to_stack.parse().unwrap();
-        stacks.move_crates(n_crates, from_stack, to_stack);
-    }
+    let (stacks, instructions): (&str, &str) = input.split_once("\n\n").unwrap();
+    let mut stacks: Dock = stacks.parse().unwrap();
+    let mut stacks2 = stacks.clone();
+
+    stacks.use_crate_mover_9000(instructions);
     let mut result = "".to_owned();
     for i in 0..stacks.stacks.len() {
-        result = result + &stacks.stacks[i].pop().unwrap();
+        result = result + &stacks.stacks[i].pop().unwrap().to_string();
     }
     println!("The top crates are (part 1): {}", result);
 
-    let (stacks, commands): (&str, &str) = input.split_once("\n\n").unwrap();
-    let mut stacks: Stacks = stacks.parse().unwrap();
-    for command in commands.trim().split("\n") {
-        let (_, split) = command.split_once("move ").unwrap();
-        let (n_crates, split) = split.split_once(" ").unwrap();
-        let n_crates: usize = n_crates.parse().unwrap();
-        let (_, split) = split.split_once("from ").unwrap();
-        let (from_stack, split) = split.split_once(" ").unwrap();
-        let from_stack: usize = from_stack.parse().unwrap();
-        let (_, to_stack) = split.split_once("to ").unwrap();
-        let to_stack: usize = to_stack.parse().unwrap();
-        stacks.move_crates2(n_crates, from_stack, to_stack);
-    }
+    stacks2.use_crate_mover_9001(instructions);
     let mut result = "".to_owned();
-    for i in 0..stacks.stacks.len() {
-        result = result + &stacks.stacks[i].pop().unwrap();
+    for i in 0..stacks2.stacks.len() {
+        result = result + &stacks2.stacks[i].pop().unwrap().to_string();
     }
     println!("The top crates are (part 2): {}", result);
 }
