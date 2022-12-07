bef97b1dfe2309208e7b3e86851829b476d28ef9
Joel Lidin <joel.lidin@volvocars.com>
2022-12-07 09:55:09 +0100

    Refactor solution for day 6
    Use bitwise operations inspired by other solutions on the internet.

--- src/day6.rs
+++ src/day6.rs
@@ -1,29 +1,32 @@
-use std::collections::HashSet;
-
 fn get_marker(n_unique: usize, input: &str) -> usize {
-    let mut tot_chars = 0;
-    let line_vec: Vec<char> = input.chars().collect();
-    for i in n_unique - 1..line_vec.len() {
-        let mut set = HashSet::new();
-        for c in line_vec[i - (n_unique - 1)..=i].iter() {
-            set.insert(c);
-        }
-        if set.len() == n_unique {
-            tot_chars += i + 1;
-            break;
-        }
-    }
-    tot_chars
+    input
+        .as_bytes()
+        .windows(n_unique)
+        .position(|bytes| {
+            bytes
+                .iter()
+                .try_fold(0usize, |mut set, char| {
+                    let prev = set;
+                    set |= 1 << (char - b'a');
+                    if prev == set {
+                        return None;
+                    }
+                    Some(set)
+                })
+                .is_some()
+        })
+        .unwrap()
+        + n_unique as usize
 }
 
 pub fn solution() {
     let input = std::fs::read_to_string("data/day6.txt").unwrap();
     println!(
-        "You need to process {} character you get 4 unique chars",
+        "You need to process {} character before you get 4 unique chars",
         get_marker(4, &input)
     );
     println!(
-        "You need to process {} character you get 14 unique chars",
+        "You need to process {} character before you get 14 unique chars",
         get_marker(14, &input)
     );
 }
