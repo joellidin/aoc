5722b568f9e70e324a11084f67ba13f8ccd8bbed
Joel Lidin <joellidin@gmail.com>
2022-12-08 16:57:24 +0100

    Refactor solution for day 7
    Inspired by other solutions, I use the fact that the tree is traversed
    in a depth first manner. With this information I could add all sizes
    from the files to the last value in the stack. Moreover, I pop the
    values, add the tuple into a "final_folders" `Vec`, and lastly add the
    size of the popped value to the parent every time we do a `$ cd ..`. The
    end result is a `Vec<(size, name)>` for all the directories in the tree.

--- src/day7.rs
+++ src/day7.rs
@@ -1,164 +1,52 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-
-#[allow(dead_code)]
-struct File {
-    parent: Option<Rc<RefCell<File>>>,
-    children: Vec<Rc<RefCell<File>>>,
-    name: String,
-    size: Option<usize>,
-}
-
-impl File {
-    fn new(name: &str, size: Option<usize>) -> Self {
-        File {
-            parent: None,
-            children: vec![],
-            name: name.to_owned(),
-            size,
-        }
-    }
-
-    fn get_dir_size(&self) -> usize {
-        match self.size {
-            Some(size) => size,
-            None => self.children.iter().fold(0usize, |acc, child| {
-                if child.borrow().children.len() == 0 {
-                    return acc + child.borrow().size.expect("All files should have sizes");
-                }
-                acc + child.borrow().get_dir_size()
-            }),
-        }
-    }
-
-    fn get_dir_size_below(&self, min_size: usize) -> usize {
-        self.children
-            .iter()
-            .filter(|child| child.borrow().children.len() > 0)
-            .fold(0usize, |acc, folder| {
-                let folder_size = folder.borrow().size.unwrap();
-                let folder_size = if folder_size <= min_size {
-                    folder_size
-                } else {
-                    0
-                };
-                acc + folder_size + folder.borrow().get_dir_size_below(min_size)
-            })
-    }
-
-    fn get_smallest_folder_size_above(&self, unused_space: usize) -> usize {
-        let disk_space_needed = 30_000_000 - unused_space;
-        self.children
-            .iter()
-            .filter(|child| {
-                child.borrow().children.len() > 0
-                    && child.borrow().size.unwrap() >= disk_space_needed
-            })
-            .map(|folder| {
-                let folder_size = folder.borrow().size.unwrap();
-                let folder_children_size =
-                    folder.borrow().get_smallest_folder_size_above(unused_space);
-                if folder_children_size < folder_size {
-                    return folder_children_size;
-                } else {
-                    return folder_size;
-                }
-            })
-            .min()
-            .unwrap_or(usize::MAX)
-    }
-
-    fn populate_dir_sizes(&mut self) {
-        if self.size.is_some() {
-            return;
-        }
-        for child in self.children.iter() {
-            child.borrow_mut().populate_dir_sizes();
-        }
-        self.size = Some(self.get_dir_size());
-    }
-
-    #[allow(dead_code)]
-    fn get_parents_depth(&self) -> usize {
-        if self.parent.is_none() {
-            return 1;
-        }
-        self.parent.as_ref().unwrap().borrow().get_parents_depth() + 1
-    }
-
-    #[allow(dead_code)]
-    fn print(&self, depth: usize) -> String {
-        let size = match &self.size {
-            Some(size) => size.to_string().to_owned(),
-            None => "Unknown".to_owned(),
-        };
-
-        return String::from("  ").repeat(depth)
-            + "- "
-            + &self.name
-            + " "
-            + &size
-            + "\n"
-            + &self
-                .children
-                .iter()
-                .map(|child| child.borrow().print(child.borrow().get_parents_depth()))
-                .collect::<Vec<String>>()
-                .join("");
-    }
-}
-
 pub fn solution() {
+    let mut dirs = vec![("/", 0usize)];
+    let mut final_dirs = vec![];
+    let mut sum_directories = 0;
+
     let input = std::fs::read_to_string("data/day7.txt").unwrap();
-    let root = Rc::new(RefCell::new(File::new("/", None)));
-    let mut current_dir = Rc::clone(&root);
-    for line in input.trim().split("\n").skip(1) {
-        let parts: Vec<&str> = line.split(" ").collect();
-        if parts.len() == 3 {
-            let directory_change = parts[2];
-            if directory_change == ".." {
-                let current_clone = Rc::clone(&current_dir);
-                current_dir = Rc::clone(current_clone.borrow().parent.as_ref().unwrap());
-                continue;
+    input
+        .trim()
+        .split("\n")
+        .skip(1)
+        .filter(|line| *line != "$ ls" && !line.starts_with("dir"))
+        .map(|line| {
+            if line.starts_with("$ cd ") {
+                let dir = &line[5..];
+                if dir == ".." {
+                    let (name, size) = dirs.pop().unwrap();
+                    if size < 100_000 {
+                        sum_directories += size;
+                    }
+                    dirs.last_mut().unwrap().1 += size;
+                    final_dirs.push((name, size))
+                } else {
+                    dirs.push((dir, 0));
+                }
+                return;
             }
-            if directory_change == "/" {
-                current_dir = Rc::clone(&root);
-                continue;
-            }
-            let child = Rc::new(RefCell::new(File::new(directory_change, None)));
-            {
-                let mut mut_child = child.borrow_mut();
-                mut_child.parent = Some(Rc::clone(&current_dir));
-            }
-            current_dir.borrow_mut().children.push(Rc::clone(&child));
-            current_dir = child;
-            continue;
-        }
-        if parts[1] == "ls" {
-            continue;
-        }
-        if parts[0] != "dir" {
-            let size = Some(parts[0].parse::<usize>().expect("Should be a number"));
-            let name = parts[1].to_owned();
-            let child = Rc::new(RefCell::new(File::new(&name, size)));
-            current_dir.borrow_mut().children.push(Rc::clone(&child));
-            {
-                let mut mut_child = child.borrow_mut();
-                mut_child.parent = Some(Rc::clone(&current_dir));
-            }
-            continue;
+            let (size, _) = line.split_once(" ").unwrap();
+            dirs.last_mut().unwrap().1 += size.parse::<usize>().unwrap();
+        })
+        .for_each(drop);
+
+    while dirs.len() > 0 {
+        let (name, size) = dirs.pop().unwrap();
+        final_dirs.push((name, size));
+        if let Some((_, _)) = dirs.last() {
+            dirs.last_mut().unwrap().1 += size;
         }
     }
-    let max_size = 100_000;
-    root.borrow_mut().populate_dir_sizes();
+
+    let needed_space = 30_000_000 - (70_000_000 - final_dirs.last().unwrap().1);
+    let smallest_dir = final_dirs
+        .iter()
+        .filter(|(_, size)| size > &needed_space)
+        .map(|(_, size)| size)
+        .min()
+        .unwrap();
     println!(
-        "Total sum of directories below {}: {}",
-        max_size,
-        root.borrow().get_dir_size_below(max_size)
+        "Total sum of directories below 100_000: {}",
+        sum_directories
     );
-    println!(
-        "Smallest directory needed to be deleted: {}",
-        root.borrow()
-            .get_smallest_folder_size_above(70_000_000 - root.borrow().size.unwrap())
-    );
+    println!("Smallest directory needed to be deleted: {}", smallest_dir);
 }
