f62290b2d3cc9e95dc469a523526a814f5b67922
Joel Lidin <joel.lidin@volvocars.com>
2022-12-12 13:49:11 +0100

    Add solution for day 12

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod day8;
 mod day9;
 mod day10;
 mod day11;
+mod day12;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -26,6 +27,7 @@ fn main() {
         "9" => day9::solution(),
         "10" => day10::solution(),
         "11" => day11::solution(),
+        "12" => day12::solution(),
         _ => panic!("Not a valid day for advent of code."),
     };
 }
