093c4e143036a866d1c122fffb36a714e78b4ac3
Joel Lidin <joel.lidin@volvocars.com>
2022-12-15 19:03:31 +0100

    Refactor day 15
    Found a much smarter way to implement this by looking around in Reddit.
    
    For part one we just check on the given row where we are looking:
    * If a point is within any of the sensors beacon distance, add one to
      the counter.
    * If a beacon is at this given point, subtract one from the counter.
    
    For part two we check every point of every sensors border (if the point
    is within the given border by the assignment). For every of these points
    we check:
    * If the point is within the beacon distance of any sensor, then we know
      that it is not the point we are looking for. Continue to the next
      point.
    * Otherwise we calculate the tuning frequency and break the program
      (should only exist one beacon satisfying the requirements).

--- src/day15.rs
+++ src/day15.rs
@@ -1,52 +1,81 @@
-use std::collections::{HashMap, HashSet};
-use std::cmp;
+use std::collections::HashSet;
 
 #[derive(Debug, Eq, PartialEq, Hash)]
 struct Point {
     x: isize,
     y: isize,
 }
-
-fn get_non_beacon_positions(pairs: &[(Point, Point)]) -> HashMap<isize, Vec<(isize, isize)>> {
-    let mut beacons = HashSet::new();
-    let mut non_beacon_min_max: HashMap<isize, Vec<_>> = HashMap::new();
-    pairs
-        .iter()
-        .map(|(sp, bp)| {
-            beacons.insert(bp);
-            let manhattan_distance = sp.x.abs_diff(bp.x) + sp.y.abs_diff(bp.y);
-            let mut width = manhattan_distance as isize;
-            let mut dy = 0;
-            while width >= 0 {
-                for y in [sp.y + dy, sp.y - dy] {
-                    let (start, end) = (sp.x - width as isize, sp.x + width as isize);
-                    if let Some(min_max_list) = non_beacon_min_max.get_mut(&y) {
-                        min_max_list.push((start, end));
-                        min_max_list.sort_unstable();
-                    } else {
-                        non_beacon_min_max.insert(y, vec![(start, end)]);
-                    }
-                }
-                dy += 1;
-                width -= 1;
-            }
-        })
-        .for_each(drop);
-    non_beacon_min_max
+impl Point {
+    fn manhattan_distance(&self, location: &Point) -> usize {
+        self.x.abs_diff(location.x) + self.y.abs_diff(location.y)
+    }
 }
 
-fn get_beacons_at(row: usize, pairs: &[(Point, Point)], (min, max): (isize, isize)) -> usize {
-    let mut beacons_at_row = HashSet::new();
-    pairs
-        .iter()
-        .filter(|(_, bp)| bp.y == row as isize && bp.x >= min && bp.x <= max)
-        .map(|(_, bp)| beacons_at_row.insert(bp))
-        .for_each(drop);
-    beacons_at_row.iter().count()
+#[derive(Debug, Eq, PartialEq, Hash)]
+struct Beacon {
+    location: Point,
+}
+
+#[derive(Debug)]
+struct Sensor {
+    location: Point,
+    beacon: Beacon,
+    beacon_distance: usize,
+}
+
+impl Sensor {
+    fn new(location: Point, beacon: Beacon) -> Self {
+        let beacon_distance = location.manhattan_distance(&beacon.location);
+        Sensor {
+            location,
+            beacon,
+            beacon_distance,
+        }
+    }
+
+    fn get_border_points(&self) -> Vec<Point> {
+        let distance = self.beacon_distance + 1;
+        let (sx, sy) = (self.location.x, self.location.y);
+        (0..distance)
+            .flat_map(|step| {
+                vec![
+                    Point {
+                        x: sx + step as isize,
+                        y: sy + step as isize - distance as isize,
+                    },
+                    Point {
+                        x: sx - step as isize,
+                        y: sy - step as isize + distance as isize,
+                    },
+                    Point {
+                        x: sx + step as isize - distance as isize,
+                        y: sy - step as isize,
+                    },
+                    Point {
+                        x: sx - step as isize + distance as isize,
+                        y: sy + step as isize,
+                    },
+                ]
+            })
+            .collect()
+    }
+
+    fn is_within_beacon_distance(&self, point: &Point) -> bool {
+        self.location.manhattan_distance(point) <= self.beacon_distance
+    }
+
+    fn get_beacons(sensors: &[Sensor]) -> HashSet<&Beacon> {
+        let mut unique_beacons = HashSet::new();
+        sensors
+            .iter()
+            .map(|s| unique_beacons.insert(&s.beacon))
+            .for_each(drop);
+        unique_beacons
+    }
 }
 
 pub fn solution() {
-    let pairs = std::fs::read_to_string("data/day15.txt")
+    let sensors = std::fs::read_to_string("data/day15.txt")
         .unwrap()
         .trim()
         .split('\n')
@@ -63,85 +92,51 @@ pub fn solution() {
                 .split_once(',')
                 .unwrap();
             let beacon_y = line.strip_prefix(" y=").unwrap();
-            (
-                Point {
-                    x: sensor_x.parse().unwrap(),
-                    y: sensor_y.parse().unwrap(),
-                },
-                Point {
+            let location = Point {
+                x: sensor_x.parse().unwrap(),
+                y: sensor_y.parse().unwrap(),
+            };
+            let beacon = Beacon {
+                location: Point {
                     x: beacon_x.parse().unwrap(),
                     y: beacon_y.parse().unwrap(),
                 },
-            )
+            };
+            Sensor::new(location, beacon)
         })
         .collect::<Vec<_>>();
 
-    let non_beacon_min_max = get_non_beacon_positions(&pairs);
-    let row = 2_000_000;
-    let min_max_list = non_beacon_min_max.get(&row).unwrap();
-    let (mut min, mut max) = (isize::MAX, isize::MIN);
-    for (mi, ma) in min_max_list.iter() {
-        if mi < &min {
-            min = *mi;
+    let beacons = Sensor::get_beacons(&sensors);
+    let y = 2_000_000;
+    let mut count = 0;
+    for x in -y..=3 * y {
+        let position = Point { x, y };
+        if sensors
+            .iter()
+            .any(|s| s.is_within_beacon_distance(&position))
+        {
+            count += 1
         }
-        if ma > &max {
-            max = *ma;
-        }
-    }
-    println!(
-        "There are {} positions that could contain a beacon",
-        (max + 1) - min - (get_beacons_at(row as usize, &pairs, (min, max)) as isize)
-    );
-
-    let (mut xx, mut yy) = (0, 0);
-    for y in 0..4_000_000 {
-        let start_end = non_beacon_min_max.get(&y).unwrap();
-        let mut range_stack = RangeStack { ranges: Vec::new() };
-        for (start, end) in start_end.iter() {
-            range_stack.add(&Range {
-                start: *start,
-                end: *end,
-            });
-        }
-        if range_stack.ranges.len() > 1 {
-            yy = y;
-            xx = range_stack.ranges[0].end + 1;
+        if beacons
+            .iter()
+            .any(|b| b.location.x == position.x && b.location.y == position.y)
+        {
+            count -= 1
         }
     }
-    println!("The tuning frequency is: {}", xx * 4000000 + yy);
-}
+    println!("There are {} positions that could contain a beacon", count);
 
-#[derive(Debug, Copy, Clone)]
-struct Range {
-    start: isize,
-    end: isize,
-}
-
-impl Range {
-    fn overlaps(&self, other: &Range) -> bool {
-        (other.start >= self.start && other.start <= self.end)
-            || (other.end >= self.start && other.end <= self.end)
-    }
-
-    fn merge(&mut self, other: &Range) {
-        self.start = cmp::min(self.start, other.start);
-        self.end = cmp::max(self.end, other.end);
-    }
-}
-
-#[derive(Debug, Clone)]
-struct RangeStack {
-    ranges: Vec<Range>,
-}
-
-impl RangeStack {
-    fn add(&mut self, range: &Range) {
-        if let Some(last) = self.ranges.last_mut() {
-            if last.overlaps(range) {
-                last.merge(range);
-                return;
+    let bound = 4_000_000;
+    for sensor in &sensors {
+        for p in sensor.get_border_points() {
+            if p.x < 0 || p.y < 0 || p.x > bound || p.y > bound {
+                continue;
             }
+            if sensors.iter().any(|s| s.is_within_beacon_distance(&p)) {
+                continue;
+            }
+            println!("The tuning frequency is: {}", p.x * 4000000 + p.y);
+            return;
         }
-        self.ranges.push(*range);
     }
 }
