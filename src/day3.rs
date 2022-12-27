ac7783d300c2b22a9c16c67ae817176edbfc3898
Joel Lidin <joel.lidin@volvocars.com>
2022-12-27 11:14:53 +0100

    Add solution for day 3

---
+++ src/day3.rs
@@ -0,0 +1,44 @@
+use std::collections::HashSet;
+
+pub fn solution() {
+    let input = std::fs::read_to_string("data/day3.txt").unwrap();
+    let mut seen_houses = HashSet::new();
+    input.chars().fold((0, 0), |(x, y), c| {
+        seen_houses.insert((x, y));
+        match c {
+            '^' => (x, y + 1),
+            'v' => (x, y - 1),
+            '>' => (x + 1, y),
+            '<' => (x - 1, y),
+            _ => (x, y),
+        }
+    });
+    println!("Santa visits {} houses", seen_houses.len());
+
+    let mut seen_houses = HashSet::new();
+    input
+        .chars()
+        .enumerate()
+        .fold(((0, 0), (0, 0)), |((xs, ys), (xr, yr)), (i, c)| {
+            if i % 2 == 0 {
+                seen_houses.insert((xs, ys));
+                match c {
+                    '^' => ((xs, ys + 1), (xr, yr)),
+                    'v' => ((xs, ys - 1), (xr, yr)),
+                    '>' => ((xs + 1, ys), (xr, yr)),
+                    '<' => ((xs - 1, ys), (xr, yr)),
+                    _ => ((xs, ys), (xr, yr)),
+                }
+            } else {
+                seen_houses.insert((xr, yr));
+                match c {
+                    '^' => ((xs, ys), (xr, yr + 1)),
+                    'v' => ((xs, ys), (xr, yr - 1)),
+                    '>' => ((xs, ys), (xr + 1, yr)),
+                    '<' => ((xs, ys), (xr - 1, yr)),
+                    _ => ((xs, ys), (xr, yr)),
+                }
+            }
+        });
+    println!("Santa and Robo-Santa visits {} houses", seen_houses.len());
+}
