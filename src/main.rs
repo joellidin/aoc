02ac83acd0ca00d0e3330dac5c6d0716e9ec3988
Joel Lidin <joel.lidin@volvocars.com>
2022-12-28 13:25:20 +0100

    Add solution for day 6

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -15,6 +16,7 @@ fn main() {
         "3" => day3::solution(),
         "4" => day4::solution(),
         "5" => day5::solution(),
+        "6" => day6::solution(),
         s => {
             let num = s.parse::<usize>().expect("Expecting a positive number");
             if num > 25 {
