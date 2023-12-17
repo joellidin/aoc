04c1d78e846d1c65b838f57f578263f2a12c1849
Joel Lidin <joellidin@gmail.com>
2023-12-17 20:07:44 +0100

    Update aoc23 day 17
    Use A* and some other smart optimizations found on reddit.

--- aoc23/src/day17.rs
+++ aoc23/src/day17.rs
@@ -1,142 +1,104 @@
-use std::collections::{BinaryHeap, HashSet};
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct Position {
     x: usize,
     y: usize,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 enum Direction {
     North,
-    East,
     South,
+    East,
     West,
+    None,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct State {
-    position: Position,
-    direction: Direction,
-    steps_in_dir: usize,
-    heat_loss: u32,
+#[derive(Debug, Clone, Copy)]
+pub struct Tile {
+    heat_loss: u8,
+    g_scores: [u16; 2],
 }
 
-impl State {
-    fn new(position: Position, direction: Direction, steps_in_dir: usize, heat_loss: u32) -> Self {
-        State {
-            position,
-            direction,
-            steps_in_dir,
-            heat_loss,
-        }
-    }
-}
-
-impl Ord for State {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other
-            .heat_loss
-            .cmp(&self.heat_loss)
-            .then_with(|| other.steps_in_dir.cmp(&self.steps_in_dir))
-    }
-}
-
-impl PartialOrd for State {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-fn dijkstra(map: &[Vec<u32>], min_steps: usize, max_steps: usize) -> u32 {
+fn a_star<const MIN_STEPS: usize, const MAX_STEPS: usize>(map: &mut [Vec<Tile>]) -> u16 {
     let mut open_set = BinaryHeap::new();
-    let mut visited = HashSet::new();
-    let directions = [
-        Direction::North,
-        Direction::East,
-        Direction::South,
-        Direction::West,
-    ];
     let goal = Position {
         y: map.len() - 1,
         x: map[0].len() - 1,
     };
 
-    // Initialize starting directions
-    for &dir in &directions {
-        open_set.push(State::new(Position { x: 0, y: 0 }, dir, 0, 0));
-    }
+    open_set.push((Reverse(0), Position { x: 0, y: 0 }, Direction::None));
+    map[0][0].g_scores = [0; 2];
 
-    while let Some(State {
-        position,
-        direction,
-        steps_in_dir,
-        heat_loss,
-    }) = open_set.pop()
-    {
+    while let Some((Reverse(g_score), position, direction)) = open_set.pop() {
         if position == goal {
-            return heat_loss;
+            return g_score;
         }
-        if !visited.insert((position, direction, steps_in_dir)) {
+        if direction != Direction::None
+            && g_score != map[position.y][position.x].g_scores[direction as usize / 2]
+        {
             continue;
         }
 
-        for &new_direction in &directions {
-            if new_direction == opposite_direction(direction)
-                || (new_direction == direction && steps_in_dir == max_steps)
-                || (new_direction != direction && steps_in_dir < min_steps)
-            {
-                continue;
+        for new_direction in match direction {
+            Direction::North | Direction::South => [Direction::East, Direction::West],
+            Direction::East | Direction::West => [Direction::South, Direction::North],
+            Direction::None => [Direction::East, Direction::South],
+        } {
+            let mut new_g_score = g_score;
+            for step in 1..=MAX_STEPS {
+                let (new_x, new_y) = match new_direction {
+                    Direction::North if position.y > step - 1 => (position.x, position.y - step),
+                    Direction::East if position.x < map[0].len() - step => {
+                        (position.x + step, position.y)
+                    }
+                    Direction::South if position.y < map.len() - step => {
+                        (position.x, position.y + step)
+                    }
+                    Direction::West if position.x > step - 1 => (position.x - step, position.y),
+                    _ => continue,
+                };
+
+                let new_position = Position { x: new_x, y: new_y };
+                let neighbour = &mut map[new_y][new_x];
+                new_g_score += neighbour.heat_loss as u16;
+                if step >= MIN_STEPS {
+                    let old_g_score = neighbour.g_scores[new_direction as usize / 2];
+
+                    if new_g_score < old_g_score {
+                        // Found a better way to this position, in this direction
+                        neighbour.g_scores[new_direction as usize / 2] = new_g_score;
+                        open_set.push((Reverse(new_g_score), new_position, new_direction))
+                    }
+                }
             }
-
-            let (new_x, new_y) = match new_direction {
-                Direction::North if position.y > 0 => (position.x, position.y - 1),
-                Direction::East if position.x < map[0].len() - 1 => (position.x + 1, position.y),
-                Direction::South if position.y < map.len() - 1 => (position.x, position.y + 1),
-                Direction::West if position.x > 0 => (position.x - 1, position.y),
-                _ => continue,
-            };
-
-            let new_steps_in_dir = if new_direction == direction {
-                steps_in_dir + 1
-            } else {
-                1
-            };
-            let new_position = Position { x: new_x, y: new_y };
-            let new_heat_loss = heat_loss + map[new_y][new_x];
-
-            open_set.push(State::new(
-                new_position,
-                new_direction,
-                new_steps_in_dir,
-                new_heat_loss,
-            ));
         }
     }
-
-    u32::MAX
+    u16::MAX
 }
 
-fn opposite_direction(dir: Direction) -> Direction {
-    match dir {
-        Direction::North => Direction::South,
-        Direction::East => Direction::West,
-        Direction::South => Direction::North,
-        Direction::West => Direction::East,
-    }
-}
-
-pub fn generator(input: &str) -> Vec<Vec<u32>> {
+pub fn generator(input: &str) -> Vec<Vec<Tile>> {
     input
         .lines()
-        .map(|l| l.chars().map(|n| n.to_digit(10).unwrap()).collect())
+        .map(|l| {
+            l.chars()
+                .map(|n| Tile {
+                    heat_loss: n.to_digit(10).unwrap() as u8,
+                    g_scores: [u16::MAX; 2],
+                })
+                .collect()
+        })
         .collect()
 }
 
-pub fn part_1(input: &[Vec<u32>]) -> u32 {
-    dijkstra(input, 0, 3)
+pub fn part_1(input: &[Vec<Tile>]) -> u16 {
+    let mut grid = input.to_owned();
+    a_star::<1, 3>(&mut grid)
 }
 
-pub fn part_2(input: &[Vec<u32>]) -> u32 {
-    dijkstra(input, 4, 10)
+pub fn part_2(input: &[Vec<Tile>]) -> u16 {
+    let mut grid = input.to_owned();
+    a_star::<4, 10>(&mut grid)
 }
