cb54c6a226b16db26882ddac76f923f899a32285
Joel Lidin <joellidin@gmail.com>
2024-12-05 12:18:08 +0100

    Optimize aoc 2024 day 5
    Use a `HashSet` instead of `Vec` inside OrderingRules. Also, utilize the
    sorting methods `is_sorted_by` for finding valid orders and `sort_by` to
    sort invalid_orders.

--- aoc24/src/day5.rs
+++ aoc24/src/day5.rs
@@ -1,47 +1,69 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
 
-type OrderingRules = HashMap<u32, Vec<u32>>;
+type OrderingRules = HashMap<u32, HashSet<u32>>;
 type PageNumbers = Vec<Vec<u32>>;
 
 pub fn generator(input: &str) -> (OrderingRules, PageNumbers) {
     let (ordering_rules_str, page_numbers_str) = input.split_once("\n\n").unwrap();
-    let mut ordering_rules = HashMap::new();
-    ordering_rules_str.lines().for_each(|line| {
-        let (left, right) = line.split_once('|').unwrap();
-        let (n, m) = (left.parse::<u32>().unwrap(), right.parse::<u32>().unwrap());
-        ordering_rules.entry(m).or_insert_with(Vec::new).push(n);
-    });
+
+    let ordering_rules =
+        ordering_rules_str
+            .lines()
+            .fold(HashMap::new(), |mut acc: HashMap<_, HashSet<_>>, line| {
+                let (left, right) = line.split_once('|').unwrap();
+                let n = left.parse::<u32>().unwrap();
+                let m = right.parse::<u32>().unwrap();
+                acc.entry(m).or_default().insert(n);
+                acc
+            });
+
     let page_numbers = page_numbers_str
         .lines()
         .map(|line| line.split(',').map(|n| n.parse().unwrap()).collect())
         .collect();
+
     (ordering_rules, page_numbers)
 }
 
-fn get_invalid_indices<T>(
-    page_numbers: &[T],
-    ordering_rules: &HashMap<T, Vec<T>>,
-) -> Option<(usize, usize)>
+fn is_valid_update<T>(page_numbers: &[T], ordering_rules: &HashMap<T, HashSet<T>>) -> bool
 where
     T: PartialEq + Eq + Hash,
 {
-    for (i, n) in page_numbers.iter().enumerate().rev() {
-        if let Some(order_numbers) = ordering_rules.get(n) {
-            for m in order_numbers.iter() {
-                if let Some(found_idx) = page_numbers.iter().skip(i).position(|k| m == k) {
-                    return Some((i, found_idx + i));
-                }
-            }
+    page_numbers.is_sorted_by(|n, m| {
+        if let Some(m_values) = ordering_rules.get(n) {
+            !m_values.contains(m)
+        } else {
+            true
         }
-    }
-    None
+    })
+}
+
+fn reorder<T>(page_numbers: &mut [T], ordering_rules: &HashMap<T, HashSet<T>>)
+where
+    T: PartialOrd + Ord + Hash,
+{
+    page_numbers.sort_by(|n, m| {
+        if let Some(m_values) = ordering_rules.get(n) {
+            if m_values.contains(m) {
+                Ordering::Greater
+            } else {
+                Ordering::Less
+            }
+        } else {
+            Ordering::Equal
+        }
+    });
 }
 
 pub fn part_1(input: &(OrderingRules, PageNumbers)) -> u32 {
     let (ordering_rules, page_numbers) = input;
     let filtered_page_numbers = page_numbers
         .iter()
-        .filter(|nums| get_invalid_indices(nums, ordering_rules).is_none());
+        .filter(|nums| is_valid_update(nums, ordering_rules));
     filtered_page_numbers
         .map(|numbers| numbers.get(numbers.len() / 2).unwrap())
         .sum()
@@ -49,22 +71,18 @@ pub fn part_1(input: &(OrderingRules, PageNumbers)) -> u32 {
 
 pub fn part_2(input: &(OrderingRules, PageNumbers)) -> u32 {
     let (ordering_rules, page_numbers) = input;
-    let mut incorrect_page_numbers = page_numbers
+    page_numbers
         .iter()
-        .filter(|nums| get_invalid_indices(nums, ordering_rules).is_some())
+        // Get all invalid page numbers
+        .filter(|nums| !is_valid_update(nums, ordering_rules))
         .cloned()
-        .collect::<Vec<_>>();
+        .fold(0, |acc, mut nums| {
+            // Sort page numbers to the correct order
+            reorder(&mut nums, ordering_rules);
 
-    incorrect_page_numbers.iter_mut().for_each(|nums| {
-        while let Some((i, j)) = get_invalid_indices(nums, ordering_rules) {
-            nums.swap(i, j);
-        }
-    });
-
-    incorrect_page_numbers
-        .iter()
-        .filter_map(|numbers| numbers.get(numbers.len() / 2))
-        .sum()
+            // Get the middle value of newly valid page numbers
+            acc + nums[nums.len() / 2]
+        })
 }
 
 #[cfg(test)]
